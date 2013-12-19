918560376251fc51d32595542c625892a1e49c28
Mike Boers <github@mikeboers.com>
2013-12-19 15:13:41 -0800

    Call hooks on a background thread

--- flask_githubhook/core.py
+++ flask_githubhook/core.py
@@ -11,7 +11,7 @@ from . import utils
 def require_from_github(func):
     @functools.wraps(func)
     def _require_from_github(*args, **kwargs):
-        res = current_app.githubhook.assert_remote_netmask()
+        res = current_app._githubhook_ext.assert_remote_netmask()
         if res:
             return res
         return func(*args, **kwargs)
@@ -35,20 +35,24 @@ def do_post():
     owner_name = payload['repository']['owner']['name']
     repo_name = payload['repository']['name']
 
-    allowed_owners = current_app.config.get('GITHUB_ALLOWED_OWNERS')
-    if allowed_owners and owner_name not in allowed_owners:
+    allowed_owners = current_app.config['GITHUB_ALLOWED_OWNERS']
+    if allowed_owners is not None and owner_name not in allowed_owners:
         return 'owner %r not permitted' % owner_name
 
-    local_path = os.path.abspath(os.path.join(
-        __file__, '..', '..', 'var', 'repos', owner_name, repo_name + '.git'
-    ))
-    remote_url = 'git@github.com:%s/%s.git' % (owner_name, repo_name)
+    clone_dir = current_app.config['GITHUB_CLONE_DIR']
+    if clone_dir is not None:
 
-    if not os.path.exists(local_path):
-        os.makedirs(local_path)
-        call(['git', 'clone', '--mirror', remote_url, local_path])
-    else:
-        call(['git', '--git-dir', local_path, 'fetch', 'origin'])
+        local_path = os.path.join(clone_dir, owner_name, repo_name + '.git')
+        remote_url = 'git@github.com:%s/%s.git' % (owner_name, repo_name)
+
+        if not os.path.exists(local_path):
+            os.makedirs(local_path)
+            call(['git', 'clone', '--mirror', remote_url, local_path])
+        else:
+            call(['git', '--git-dir', local_path, 'fetch', 'origin'])
+
+    for hook in current_app._githubhooks:
+        hook(payload)
 
     return 'ok'
 
@@ -61,11 +65,22 @@ class GitHubHook(object):
         self.init_app(app, **kwargs)
 
     def init_app(self, app, **kwargs):
-        app.githubhook = self
+
+        app._githubhook_ext = self
+
+        app._githubhooks = []
+        def githubhook(func):
+            app._githubhooks.append(func)
+        app.githubhook = githubhook
+
         app.register_blueprint(blueprint, **kwargs)
+
+        app.config.setdefault('GITHUB_ALLOWED_OWNERS', None)
+        app.config.setdefault('GITHUB_CLONE_DIR', os.path.join(app.instance_path, 'repos'))
 
     def assert_remote_netmask(self):
         if not current_app.debug and not utils.addr_in_network(request.remote_addr, self.remote_netmask):
             return 'You are not GitHub.'
+
 
 
